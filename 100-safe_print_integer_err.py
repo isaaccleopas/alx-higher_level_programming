e74694ceb524869d570fa2c1867cd58494246623
isaaccleopas <isaaccleopas@gmail.com>
2022-12-21 15:45:57 +0000

    task 100 added

--- 100-safe_print_integer_err.py
+++ 100-safe_print_integer_err.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
-    except (ValueError, TypeError) as error:
-        import sys
-        print("Exception: {}".format(error), file=sys.stderr)
-        return False
-    else:
-        return True
+    except (ValueError, TypeError) as err:
+        sys.stderr.write("Exception: {}\n".format(err))
+        return(False)
+    return(True)

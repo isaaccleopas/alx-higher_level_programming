0780050d9aa4306fe0c43aeab1f06cebebf52974
isaaccleopas <isaaccleopas@gmail.com>
2023-01-09 12:54:55 +0000

    task 11 added

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 """
-Contain the class BaseGeometry and subclass Rectangle
+Contains the class BaseGeometry and subclass Rectangle
 """
+
+
 class BaseGeometry:
     """A class with public instance methods area and integer_validator"""
     def area(self):
         """raises an exception when called"""
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """validates that value is an integer greater than 0"""
         if type(value) is not int:
             raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
+
 
 class Rectangle(BaseGeometry):
     """A representation of a rectangle"""
@@ -22,6 +26,15 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """informal string representation of the rectangle"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
 
 class Square(Rectangle):
     """A representation of a square"""
